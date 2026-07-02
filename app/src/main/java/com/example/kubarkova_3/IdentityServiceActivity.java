767a3f675a6cfb83666ae9b0fbefcf345ff8abb4
lidjfnaku <lidjfnaku@gmail.com>
2026-07-02 11:37:54 +0700

    init: create project with 3 activities

---
+++ app/src/main/java/com/example/kubarkova_3/IdentityServiceActivity.java
@@ -0,0 +1,7 @@
+public class IdentityServiceActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identity_service); // ← проверьте
+    }
+}
---
+++ app/src/main/java/com/example/kubarkova_3/NoticeActivity.java
@@ -0,0 +1,7 @@
+public class NoticeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notice); // ← проверьте
+    }
+}
